6ed54173a113fb6ec27c23b99b6f0e22daa42a72
alnicolaou96 <alnicolaou96@gmail.com>
2018-05-31 12:01:06 -0400

    multiples of 5

--- FizzBuzz.Tests/FizzBuzzerTests.cs
+++ FizzBuzz.Tests/FizzBuzzerTests.cs
@@ -19,5 +19,51 @@ namespace FizzBuzz.Tests
 
             Assert.That(result, Is.EqualTo("1"));
         }
+        [Test]
+        public void Get_Returns_2_for_2()
+        {
+            var fizzBuzzer = new FizzBuzzer();
+
+            var result = fizzBuzzer.Get(2);
+
+            Assert.That(result, Is.EqualTo("2"));
+        }
+        [Test]
+        public void Get_Returns_Fizz_for_3()
+        {
+            var fizzBuzzer = new FizzBuzzer();
+
+            var result = fizzBuzzer.Get(3);
+
+            Assert.That(result, Is.EqualTo("Fizz"));
+        }
+        [Test]
+        public void Get_Returns_four_mul_3()
+        {
+            var fizzBuzzer = new FizzBuzzer();
+
+            var result = fizzBuzzer.Get(6);
+
+            Assert.That(result, Is.EqualTo("Fizz"));
+        }
+        [Test]
+        public void Get_Returns_4_for_4()
+        {
+            var fizzBuzzer = new FizzBuzzer();
+
+            var result = fizzBuzzer.Get(4);
+
+            Assert.That(result, Is.EqualTo("4"));
+        }
+        [Test]
+        public void Get_Returns_Buzz_for_mul_5()
+        {
+            var fizzBuzzer = new FizzBuzzer();
+
+            var result = fizzBuzzer.Get(10);
+
+            Assert.That(result, Is.EqualTo("Buzz"));
+        }
+
     }
 }
